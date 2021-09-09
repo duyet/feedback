5d32d1e039c10101cb4d3d7a4adbdc85a7d0f6b5
Duyet Le <lvduyet@fossil.com>
2021-09-09 22:44:26 +0700

    chore: add splitbee

--- dashboard/pages/_app.tsx
+++ dashboard/pages/_app.tsx
@@ -1,7 +1,16 @@
+import splitbee from '@splitbee/web';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: '/bee.js',
+      apiUrl: '/_hive',
+    });
+  });
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
