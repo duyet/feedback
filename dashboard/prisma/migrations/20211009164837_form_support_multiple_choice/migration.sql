ea88484e718dca88777eeb5cbb40113edaad3172
Duyet Le <5009534+duyet@users.noreply.github.com>
2021-10-13 17:33:00 +0700

    Update migration.sql

--- dashboard/prisma/migrations/20211009164837_form_support_multiple_choice/migration.sql
+++ dashboard/prisma/migrations/20211009164837_form_support_multiple_choice/migration.sql
@@ -3,4 +3,4 @@ ALTER TABLE "Form" ADD COLUMN     "hasOtherChoice" BOOLEAN NOT NULL DEFAULT fals
 ADD COLUMN     "isMultipleChoice" BOOLEAN NOT NULL DEFAULT false;
 
 -- RenameIndex
-ALTER INDEX "ProjectSetting_projectId_unique" RENAME TO "ProjectSetting_projectId_key";
+ALTER INDEX IF EXISTS "ProjectSetting_projectId_unique" RENAME TO "ProjectSetting_projectId_key";
